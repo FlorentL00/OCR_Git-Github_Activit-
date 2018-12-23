225e8f754509db83116844c68aa2aec521fe4c5c
FlorentL00 <florent0710@gmail.com>
2018-12-23 19:15:17 +0100

    Modification de TemperatureConversion et ajout du code

--- TempératureConversion.java
+++ TempératureConversion.java
@@ -1,1 +1,16 @@
 // Voici la methode TemperatureConversion
+
+public class TempératureConversion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ConvertisseurTempératures maTempérature = new ConvertisseurTempératures();
+		
+		maTempérature.convertirTempérature(10, 'C');
+		maTempérature.convertirTempérature(50, 'F');
+		maTempérature.convertirTempérature(283.15, 'K');
+
+	}
+
+}
